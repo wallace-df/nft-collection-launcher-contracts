89a7273727f944041a05c3066312ba5fe57c29a8
wallace.d.f@hotmail.com <wallaceferreira19@gmail.com>
2022-07-28 16:18:13 -0300

    Fixes.

--- cairo/compile_cairo_contracts.sh
+++ cairo/compile_cairo_contracts.sh
@@ -3,9 +3,9 @@ mkdir -p artifacts/compiled
 mkdir -p artifacts/abis
 cd contracts
 
-# starknet-compile NFTBaseCollection.cairo \
-#     --output ../artifacts/compiled/NFTBaseCollection.json \
-#     --abi ../artifacts/abis/NFTBaseCollection.json
+starknet-compile NFTBaseCollection.cairo \
+    --output ../artifacts/compiled/NFTBaseCollection.json \
+    --abi ../artifacts/abis/NFTBaseCollection.json
 
 starknet-compile NFTCollectionLauncher.cairo \
     --output ../artifacts/compiled/NFTCollectionLauncher.json \
