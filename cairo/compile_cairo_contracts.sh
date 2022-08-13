d52e7b67779cae2f6cc4e02eaf1734368b62558f
wallace.d.f@hotmail.com <wallaceferreira19@gmail.com>
2022-08-13 16:51:27 -0300

    Fixes

--- cairo/compile_cairo_contracts.sh
+++ cairo/compile_cairo_contracts.sh
@@ -7,6 +7,10 @@ starknet-compile NFTBaseCollection.cairo \
     --output ../artifacts/compiled/NFTBaseCollection.json \
     --abi ../artifacts/abis/NFTBaseCollection.json
 
+starknet-compile NFTDelegatableCollection.cairo \
+    --output ../artifacts/compiled/NFTDelegatableCollection.json \
+    --abi ../artifacts/abis/NFTDelegatableCollection.json
+
 starknet-compile NFTCollectionLauncher.cairo \
     --output ../artifacts/compiled/NFTCollectionLauncher.json \
     --abi ../artifacts/abis/NFTCollectionLauncher.json
--- cairo/deploy_cairo_contracts.sh
+++ cairo/deploy_cairo_contracts.sh
@@ -2,3 +2,7 @@ set -e
 output=`starknet declare --network=alpha-goerli --contract ./artifacts/compiled/NFTBaseCollection.json`
 contract_hash_class=${output:51:65}
 starknet deploy --network=alpha-goerli --contract ./artifacts/compiled/NFTCollectionLauncher.json --inputs  ${contract_hash_class} --no_wallet
+
+output=`starknet declare --network=alpha-goerli --contract ./artifacts/compiled/NFTDelegatableCollection.json`
+contract_hash_class=${output:51:65}
+starknet deploy --network=alpha-goerli --contract ./artifacts/compiled/NFTCollectionLauncher.json --inputs  ${contract_hash_class} --no_wallet
